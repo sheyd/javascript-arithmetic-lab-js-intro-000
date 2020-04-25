0ee8307b1b9e497b3b208aa4e8ae82cb787a65cd
Sena Heydari <sheydari@dosomething.org>
2020-04-25 02:55:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function add () {};
 function subtract () {};
 function multiply() {};
 function divide() {};
+
+function increment(n) {
+  return n += 1;
+}
+
+function decrement(n) {
+  return n -= 1;
+}
